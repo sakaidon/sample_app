34f616ddc001ae0a9efb1784a074ef10b3d7de97
sakaiNFIG <jmjm_jmjm@me.com>
2020-04-15 12:37:07 +0000

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,13 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALD_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
   has_secure_password
-
   validates :password, presence: true, length: { minimum: 6 }
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
